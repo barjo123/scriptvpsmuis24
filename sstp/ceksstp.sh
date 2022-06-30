ddc5ae12599f8119ac8cf9ddf250c2c1a8989c72
Kelvin Tan <kelvintan1101@gmail.com>
2022-06-30 22:05:10 +0800

    Create ceksstp.sh

---
+++ sstp/ceksstp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/kelvintan1101/permission/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+exit 0
+fi
+clear
+accel-cmd show sessions
+echo ""
